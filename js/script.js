cc6053849b0fc977aa6272164cc9def02e4612c4
Dayo Farayola <dayojnr.3@gmail.com>
2023-10-25 00:51:00 +0100

    navbar fix

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 $(document).ready(function() {
 
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 20,
-        nav: false,
-        dots: true,
-        responsive: {
-          511: {
-            items: 1
-          },
-          768: {
-            items: 2
-          },
-          1000: {
-            items: 4
-          }
-        }
-    })
-
-
-    let sideNav = $("#mySidenav");
-    let hamburgerIcon = $(".hamburger-icon");
-    let closeBtn = $(".closebtn");
-
-    function openNav() {
-        sideNav.css("width", "95%");
-        hamburgerIcon.addClass("close");
-        closeBtn.addClass("open");
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    responsive: {
+      511: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
     }
+  })
 
-    function closeNav() {
-        sideNav.css("width", "0");
-        hamburgerIcon.removeClass("close");
-        closeBtn.removeClass("open");
-    }
 
-    // Attach click event handlers to your elements
-    hamburgerIcon.click(openNav);
-    closeBtn.click(closeNav);
+  let sideNav = $("#mySidenav");
+  let hamburgerIcon = $(".hamburger-icon");
+  let closeBtn = $(".closebtn");
+
+  function openNav() {
+      sideNav.css("width", "95%").addClass("open");
+      hamburgerIcon.addClass("close");
+      closeBtn.addClass("open");
+  }
+
+  function closeNav() {
+      sideNav.css("width", "0");
+      hamburgerIcon.removeClass("close");
+      closeBtn.removeClass("open");
+  }
+
+  // Attach click event handlers to your elements
+  hamburgerIcon.click(openNav);
+  closeBtn.click(closeNav);
 });
