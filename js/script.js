2d762bc503bd7e1d41999d5a27bdd456cd06ca2d
Dayo Farayola <dayojnr.3@gmail.com>
2023-11-19 16:00:33 +0100

    changes made to layout and added functionality

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
         items: 2
       },
       1000: {
-        items: 5
+        items: 4
       }
     }
   });
@@ -23,19 +23,38 @@ $(document).ready(function() {
   let hamburgerIcon = $(".hamburger-icon");
   let closeBtn = $(".closebtn");
 
-  function openNav() {
-      sideNav.css("width", "95%").addClass("open");
-      hamburgerIcon.addClass("close");
-      closeBtn.addClass("open");
+  const openNav = () => {
+    sideNav.css("width", "95%").addClass("open");
+    hamburgerIcon.addClass("close");
+    closeBtn.addClass("open");
   }
 
-  function closeNav() {
-      sideNav.css("width", "0");
-      hamburgerIcon.removeClass("close");
-      closeBtn.removeClass("open");
+  const closeNav = () => {
+    sideNav.css("width", "0");
+    hamburgerIcon.removeClass("close");
+    closeBtn.removeClass("open");
   }
 
   // Attach click event handlers to your elements
   hamburgerIcon.click(openNav);
   closeBtn.click(closeNav);
+
+  // Define an array of strings
+  const changeSignBoard = ['by!', 'buy!', 'bye!'];
+  let currentIndex = 0;
+
+  //Set the text content of the span element to each string in the array, one after the other
+  setInterval(() => {
+    $(".stopSign").text(changeSignBoard[currentIndex % changeSignBoard.length]);
+    currentIndex++;
+  }, 2000);
+
+  //set the current location to the selected option
+  $(".more_location_select").change(function () {
+    // Capture the selected value from select
+    let selectedValue = $(this).val(); 
+    // Set the text content of .current-location to the selected value
+    $(".current-location").text(selectedValue); 
+  });
+
 });
